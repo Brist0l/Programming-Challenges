3eff3ecedcfc1d76988d06aabcaa995dcf45e89f
Brist0l <gautamthegreat21@gmail.com>
2022-12-26 12:51:53 +0530

    Fixed my silly mistake

--- v3/107.py
+++ v3/107.py
@@ -1,5 +1,11 @@
 import random
 
-x = random.randint(100,1000)
-y = random.randint(100,1000)
-print(f"x = {x}\ny = {y}\nx*y={x*y}")
+first_100_digit = ''*100
+second_100_digit = ''*100
+while len(first_100_digit) != 100:
+    x = random.randint(0,10)
+    y = random.randint(0,10)
+    first_100_digit += str(x) 
+    second_100_digit += str(y)
+
+print(f"x = {first_100_digit}\ny = {second_100_digit}\nx*y={int(first_100_digit)*int(second_100_digit)}")
